34398a18c4601b111fc8cc943075edc06d3df934
hugosenari <hugosenari@gmail.com>
2022-03-22 01:10:08 -0300

    chore: re-index ../nimpkgs/i/impulse_engine

--- master/flake.nix
+++ master/flake.nix
@@ -8,8 +8,8 @@
   inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
   
   inputs.src-impulse_engine-master.flake = false;
+  inputs.src-impulse_engine-master.ref   = "refs/heads/master";
   inputs.src-impulse_engine-master.owner = "matkuki";
-  inputs.src-impulse_engine-master.ref   = "master";
   inputs.src-impulse_engine-master.repo  = "Nim-Impulse-Engine";
   inputs.src-impulse_engine-master.type  = "github";
   
